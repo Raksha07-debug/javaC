4e0bc820dc8ef3a8ddedb06c9dc12fcb3b9b3816
Raksha <rak070803@gmail.com>
2025-01-07 13:00:40 +0530

    done add

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 class Calculator {
 public static void main(String args[]){
+Scanner scanner = new Scanner(System.in);
 System.out.println("welcome to calculator");
 
+char operator = scanner.next().charAt(0);
+double num1 = scanner.nextDouble();
+double num2  = scanner.nextDouble();
+double result;
+
+switch(operator){
+case'+':
+result=num1+num2;
+break;
+
 
 }
-public static  int div( int a, int b){
- return a/b;
-}
+
+
+
 }
